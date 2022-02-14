97f62af8b346b84f46dc4bcc93d95f28f1399128
ojo-perezoso <bymartin24@gmail.com>
2022-02-14 14:11:39 -0800

    changed quick_sort completly but it doesnt work

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,69 +1,101 @@
 #include "sort.h"
 #include <stdio.h>
 
+size_t quick_partition(int *array, size_t low, size_t high, size_t size);
+void quick_swap(int *x, int *y);
+void quick_recursion(int *array, size_t low, size_t high, size_t size);
+
 /**
- * quick_partition - makes pivot and sorts for quick recursion
+ * quick_swap - swaps to items in an array
  *
- * @array: array to sort
- * @low: start of list
- * @high: end of list
+ * @x: poiner to the first item
+ * @y: pointer to the second item
  */
-void quick_partition(int *array, size_t low, size_t high, size_t size)
+void quick_swap(int *x, int *y)
 {
-	size_t pivot, idx_min, idx_right;
 	int aux;
 
-	if (low >= high - 1|| high < 2)
-		return;
+	aux = *x;
+	*x = *y;
+	*y = aux;
+}
 
-	pivot = high - 1;
-	if (pivot == low)
-		return;
+/**
+ * quick_recursion - recursive auxliary function for quick sort
+ *
+ * @array: array to sort recurively
+ * @low: starting point to sort
+ * @high: index at which to stop
+ * @size: size of the entire array
+ */
+void quick_recursion(int *array, size_t low, size_t high, size_t size)
+{
+	size_t idx_pivot;
+	
+	if (low < high)
+	{
+		/*printf("entered if because: %ld < %ld\n", low, high);*/
+		idx_pivot = quick_partition(array, low, high, size);
+/*		printf("idx_pivot: %ld: %d\n", idx_pivot, array[idx_pivot]);*/
+		if (idx_pivot > 0)
+			quick_recursion(array, low, (idx_pivot - 1), size);
+		if (idx_pivot < high)
+			quick_recursion(array, (idx_pivot + 1), high, size);
+	}
+}
+
+/**
+ * quick_partition - recursive functionality for quick sort
+ * partitions a list of integers based on the pivot
+ *
+ * @array: array to sort
+ * @low: start of the sort
+ * @high: end of the sort
+ * @size: size of the entire array
+ *
+ * Return: the index of the pivot
+ */
+size_t quick_partition(int *array, size_t low, size_t high, size_t size)
+{
+	size_t idx_pivot, idx_min, idx_right;
+
+	idx_pivot = high;
 	idx_min = low - 1;
+
 	for (idx_right = low; idx_right < high; idx_right++)
 	{
-		if (array[idx_right] < array[pivot])
+		if (array[idx_right] < array[idx_pivot])
 		{
-			/* swaps */
 			idx_min++;
 			if (idx_min != idx_right)
 			{
-				aux = array[idx_right];
-				array[idx_right] = array[idx_min];
-				array[idx_min] = aux;
-				printf("\n***Dentro for***\n");
+				quick_swap(&array[idx_min], &array[idx_right]);
+
 				print_array(array, size);
 			}
 		}
 	}
-	/* swaps the pivot to idx_small + 1
-	printf("high - 2: %ld idx_min: %ld\n", high, idx_min);*/
-	if (high != idx_min || pivot == idx_min + 1)
+	if (array[idx_pivot] < array[idx_min])
 	{
+		printf("idx_min != pivot (%ld != %ld)\n", idx_min, idx_pivot);
+		quick_swap(&array[idx_pivot], &array[idx_min]);
 
-		aux = array[idx_min + 1];
-		array[idx_min + 1] = array[pivot];
-		array[pivot] = aux;
-		/* calling recurively */
-		printf("\n***Fuera del FOR***\n");
 		print_array(array, size);
-		/*printf("ISZQUIERA %d to %d\n", array[idx_min + 1], array[pivot - 1]);*/
-		quick_partition(array, low, idx_min + 1, size);
-		/*printf("DESRECHLEAR %d to %d\n", array[pivot + 1], array[size - 1]);*/
-		quick_partition(array, idx_min + 2, size, size);
 	}
+
+	return (idx_min + 1);
+	
 }
+
 /**
- * quick_sort - sorts an array of integers using quick sort recurisvely
+ * quick_sort - sorts an array of ints using quick sort
  *
  * @array: array to sort
  * @size: size of the array
  */
 void quick_sort(int *array, size_t size)
 {
+	if (array && size > 1)
+		quick_recursion(array, 0, (size - 1), size);
 
-	if (size < 2)
-		return;
-
-	quick_partition(array, 0, size, size);
 }
